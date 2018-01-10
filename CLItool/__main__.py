e31cf42b7c30d8918a00f3ac4f0f21d19320b8d1
Molex <simplesleeptimer@gmail.com>
2018-01-10 22:47:44 +0100

    Begin working on getting graph from API and displaying it

--- CLItool/__main__.py
+++ CLItool/__main__.py
@@ -3,6 +3,7 @@ from requests.exceptions import InvalidURL
 import json
 import sys
 import datetime
+import argparse
 
 
 def convert_unix(unix_stamp, _format):
@@ -10,22 +11,55 @@ def convert_unix(unix_stamp, _format):
     return datetime.datetime.fromtimestamp(unix_stamp).strftime(_format)
 
 
-API_IP = "192.168.1.250"
-API_PORT = "2020"
-
-if __name__ == '__main__':
-    print('Requesting data from API...')
+def get_newest_data():
+    """
+    Request the most recent data set from the api
+    :return: dictionary with temp, humid, pressure and time
+    """
     try:
-        res = requests.get('http://{}:{}/climate/now'.format(API_IP, API_PORT), timeout=10)
+        res = requests.get('http://{}:{}/climate/now'.format(API_IP, API_PORT),
+                           timeout=10)
         res.raise_for_status()
     except InvalidURL:
-        sys.exit('Invalid URL, most likely a problem with the API_IP or API_PORT constants in __main__.py')
+        sys.exit('Invalid URL, most likely a problem with the API_IP or '
+                 'API_PORT constants in __main__.py')
     except Exception as e:
         print('Something went wrong when connecting to API...')
         raise e
 
-    data = json.loads(res.text)
-    print('\n{}: {}\'C | {}% | {} millibars'.format(convert_unix(data['time'], '%d/%m/%Y %H:%M:%S'),
-                                                    round(data['temp'], 1),
-                                                    round(data['humid'], 1),
-                                                    round(data['pressure'], 1)))
+    return json.loads(res.text)
+
+
+def get_graph(start):
+    try:
+        res = requests.get('http://{}:{}/graph'.format(API_IP, API_PORT),
+                           timeout=10, data={'start_time': start})
+        res.raise_for_status()
+    except InvalidURL:
+        sys.exit('Invalid URL, most likely a problem with the API_IP or '
+                 'API_PORT constants in __main__.py')
+    except Exception as e:
+        print('Something went wrong when connecting to API...')
+        raise e
+    # TODO: return as object suited for tkinter
+
+
+API_IP = "192.168.1.250"
+API_PORT = "2020"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get info from EnviroSense')
+    parser.add_argument('-d', '--days', type=float, default=None)
+    parser.add_argument('-hr', '--hours', type=float, default=None)
+    args = parser.parse_args()
+
+    if args.days or args.hours:
+        # TODO: Get graph from API and display it with tkinter
+        pass
+    else:
+        print('Requesting data from API...')
+        data = get_newest_data()
+        print('\n{}: {}\'C | {}% | {} millibars'.format(convert_unix(data['time'], '%d/%m/%Y %H:%M:%S'),
+                                                        round(data['temp'], 1),
+                                                        round(data['humid'], 1),
+                                                        round(data['pressure'], 1)))
