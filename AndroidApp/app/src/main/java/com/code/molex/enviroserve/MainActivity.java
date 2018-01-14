b6940b64c994a10bd64e26d4b73e33e0210e96c0
Molex <simplesleeptimer@gmail.com>
2018-01-14 21:53:35 +0100

    Refactor android code

--- AndroidApp/app/src/main/java/com/code/molex/enviroserve/MainActivity.java
+++ AndroidApp/app/src/main/java/com/code/molex/enviroserve/MainActivity.java
@@ -16,6 +16,8 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
     private final String errorString = "Error";
 
@@ -25,11 +27,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public HashMap<String, String> formatData(JSONObject response) throws JSONException{
+        HashMap<String, String> data = new HashMap<>();
+        // Extract data from JSON
+        Long temperature = Math.round(response.getDouble("temp"));
+        Long humidity = Math.round(response.getDouble("humid"));
+        Long pressure = Math.round(response.getDouble("pressure"));
+        // Format it
+        data.put("temp", temperature.toString() + getString(R.string.temp_unit));
+        data.put("humid", humidity.toString() + getString(R.string.humid_unit));
+        data.put("pressure", pressure.toString() + getString(R.string.pressure_unit));
+        return data;
+
+    }
+
     public void refreshData(View view){
+        // Get text placeholders
         final TextView tempText = findViewById(R.id.temp);
         final TextView humidText = findViewById(R.id.humid);
         final TextView pressureText = findViewById(R.id.pressure);
 
+        // Prepare for sending request
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "http://192.168.1.250:2020/climate/now";
 
@@ -40,20 +58,13 @@ public class MainActivity extends AppCompatActivity {
                         try{
                             // Extract values from JSON
                             Log.i("Response content", response.toString());
-                            Long temperature = Math.round(response.getDouble("temp"));
-                            Long humidity = Math.round(response.getDouble("humid"));
-                            Long pressure = Math.round(response.getDouble("pressure"));
-                            String tempString = temperature.toString() + getString(R.string.temp_unit);
-                            String humidString = humidity.toString() + getString(R.string.humid_unit);
-                            String pressureString = pressure.toString() + getString(R.string.pressure_unit);
-                            Log.i("Temp String", tempString);
-                            Log.i("Humid String", humidString);
-                            Log.i("Pressure String", pressureString);
+                            HashMap<String, String> data = formatData(response);
                             // Set text boxes
-                            tempText.setText(tempString);
-                            humidText.setText(humidString);
-                            pressureText.setText(pressureString);
+                            tempText.setText(data.get("temp"));
+                            humidText.setText(data.get("humid"));
+                            pressureText.setText(data.get("pressure"));
                         } catch (JSONException e) {
+                            // TODO: Show a dialog box
                             tempText.setText(errorString);
                         }
 
@@ -61,10 +72,10 @@ public class MainActivity extends AppCompatActivity {
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-
+                        // TODO: Smart error things
                     }
                 });
-
+        // Launch.
         queue.add(dataRequest);
     }
 }
