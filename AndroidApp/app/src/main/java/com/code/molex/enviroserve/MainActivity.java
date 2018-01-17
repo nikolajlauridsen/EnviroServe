c21999bc36b19cd560eafc71509cd40c8f7b5301
Molex <simplesleeptimer@gmail.com>
2018-01-17 23:43:31 +0100

    Add initial graphing prototype

--- AndroidApp/app/src/main/java/com/code/molex/enviroserve/MainActivity.java
+++ AndroidApp/app/src/main/java/com/code/molex/enviroserve/MainActivity.java
@@ -1,5 +1,6 @@
 package com.code.molex.enviroserve;
 
+import android.graphics.Color;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,7 +14,11 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateGraph();
     }
 
 
@@ -36,6 +42,60 @@ public class MainActivity extends AppCompatActivity {
         refreshData(getWindow().getDecorView().getRootView());
     }
 
+    }
+
+    private void populateGraph(){
+        // Get data
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.250:2020/climate/data";
+
+        JsonObjectRequest dataRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Log.i("Graph", "Graphing data...");
+                            JSONArray data = (JSONArray) response.get("results");
+                            DataPoint[] dataPoints = new DataPoint[data.length()];
+
+                            for (int i=0; i < data.length(); i++){
+                                JSONObject datapoint = data.getJSONObject(i);
+
+                                dataPoints[i] = new DataPoint(datapoint.getDouble("time"),
+                                                              datapoint.getDouble("temp"));
+                            }
+
+                            LineGraphSeries<DataPoint> tempSeries = new LineGraphSeries<>(dataPoints);
+                            // Get Add the series to the graph
+                            GraphView graph = findViewById(R.id.graph);
+
+                            // Enable scroll and zoom
+                            // set manual X bounds
+                            graph.getViewport().setYAxisBoundsManual(true);
+                            graph.getViewport().setMinY(-150);
+                            graph.getViewport().setMaxY(150);
+
+                            graph.getViewport().setXAxisBoundsManual(true);
+                            graph.getViewport().setMinX(4);
+                            graph.getViewport().setMaxX(80);
+
+                            // enable scaling and scrolling
+                            graph.getViewport().setScalable(true);
+                            graph.getViewport().setScalableY(true);
+
+                            tempSeries.setColor(Color.RED);
+                            graph.addSeries(tempSeries);
+                        } catch (JSONException e){
+                            // TODO: Something clever
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+        queue.add(dataRequest);
     }
 
     public HashMap<String, String> formatData(JSONObject response) throws JSONException{
