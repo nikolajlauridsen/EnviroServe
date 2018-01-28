b7a96425281b6db810edd0c68a09a9c4ec06e4a5
Molex <simplesleeptimer@gmail.com>
2018-01-28 01:41:39 +0100

    Increase sensor resolution to every 2 minutes

--- Sensor/__main__.py
+++ Sensor/__main__.py
@@ -3,7 +3,7 @@ import time
 import requests
 from requests import ConnectionError, Timeout
 
-RESOLUTION = 60*5      # How often to report in on environmental stats.
+RESOLUTION = 60*2     # How often to report in on environmental stats.
 HUMID_WARN = 70        # Humidity to warn at.
 HUMID_PAUSE = 60*30   # How long to wait after humid warning is dismissed.
 
