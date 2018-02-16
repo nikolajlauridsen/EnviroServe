931e184f18e6bd2f44d47a5a32a3d66e404e01e4
Molex <simplesleeptimer@gmail.com>
2018-02-16 20:38:55 +0100

    Add inital code for LCD screen

--- Arduino Sensor/uno_sensor_slave/src/uno_sensor_slave.ino
+++ Arduino Sensor/uno_sensor_slave/src/uno_sensor_slave.ino
@@ -3,6 +3,7 @@
 #include <DHT_U.h>
 
 #include <Wire.h>
+#include <LiquidCrystal.h>
 
 #define DEBUG true
 
@@ -11,11 +12,20 @@
 #define DHTPIN 2
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 const byte slaveID = 2;
+const byte leftBtn = 3;
+const byte centerBtn = 4;
+const byte rightBtn = 5;
+const byte lcdBackLight = 6;
+bool lcdState;
+bool centerState = false;
 unsigned long int dhtTime;
+unsigned long int goTime;
 // The DHT needs a cool down period of at least 1 second.
 int dhtInterval = 2000;
+int idleTime = 1000;
 
 void setup() {
   if (DEBUG){
@@ -25,12 +35,21 @@ void setup() {
   // Set up i/o
   pinMode(lightPin, INPUT);
   pinMode(GAS, INPUT);
+  pinMode(centerBtn, INPUT);
+  pinMode(lcdBackLight, OUTPUT);
+  digitalWrite(lcdBackLight, HIGH);
+  lcdState = true;
   // i2c as slave
   Wire.begin(slaveID);
   Wire.onRequest(requestEvent);
   // dht sensor
   dht.begin();
   dhtTime = millis();
+  goTime = millis();
+  // Screen
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.write("Hello world!");
   if (DEBUG){
     Serial.println("Done!");
   }
@@ -43,12 +62,36 @@ float light_scale;
 int humid, temp, smoke, smoke_perc;
 
 void loop() {
-  readSensors();
-  // Print to console
-  if (DEBUG){
-      debugPrint();
+  // idle job
+  if (millis() >= goTime){
+    readSensors();
+    updateScreen();
+    // Print to console
+    if (DEBUG){
+        debugPrint();
+    }
+    goTime = millis() + idleTime;
   }
-  delay(500);
+
+  // On-demand
+  bool center_pushed = digitalRead(centerBtn);
+  if (center_pushed == LOW){
+      if (!centerState){
+          // Center state was false, ie. the button has just been pressed
+          // Check screen state and switch it
+          if(lcdState){
+              digitalWrite(lcdBackLight, LOW);
+              lcdState = false;
+          } else {
+              digitalWrite(lcdBackLight, HIGH);
+              lcdState = true;
+          }
+          centerState = true;
+      }
+  } else {
+      centerState = false;
+  }
+
 }
 
 void requestEvent(){
@@ -94,6 +137,22 @@ void readSensors(){
   smoke = analogRead(GAS);
   float smoke_scalar = smoke / 1024.0;
   smoke_perc = smoke_scalar * 100;
+}
+
+void updateScreen(){
+    lcd.setCursor(0,0);
+    lcd.print("Temp|Light|Smoke");
+    lcd.setCursor(1, 1);
+    lcd.print(round(temp));
+    lcd.setCursor(6, 1);
+    lcd.print(round(light_perc));
+    lcd.print('%');
+    lcd.setCursor(12, 1);
+    lcd.print(smoke);
+    if (smoke < 100){
+        lcd.print(' ');
+    }
+
 }
 
 void debugPrint(){
